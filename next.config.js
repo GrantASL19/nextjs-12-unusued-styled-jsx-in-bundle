8c7e48f96b57a7529de4e5da5647a678e48972fa
Grant Heaslip <grant@asl19.org>
2021-10-26 17:21:20 -0400

    Add Webpack Bundle Analyzer and run on client build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static",
+          reportFilename: options.isServer
+            ? "../analyze/server.html"
+            : "./analyze/client.html",
+        }),
+      );
+    }
+
+    return config;
+  }
+};
